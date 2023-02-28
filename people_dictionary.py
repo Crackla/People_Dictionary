b2efbce79773199843ad62f69306e72ab9e169fa
Crackla <122633839+Crackla@users.noreply.github.com>
2023-02-28 11:13:21 +0100

    Update people_dictionary.py
    -Added function to handle integer inputs
    -Use generated numbers instead of names as keys in the dictionary

--- people_dictionary.py
+++ people_dictionary.py
@@ -3,66 +3,93 @@ dictionary = {}
 
 def add_data():
     name_add = input("name: ")
-    age_add = int(input("age: "))
+    age_add = int_input("age: ")
     address_add = input("address: ")
-    new_person = {"age": age_add, "address": address_add}
-    dictionary[name_add] = new_person
+    new_person = {"name": name_add, "age": age_add, "address": address_add}
+    numbers = len(dictionary.keys())
+    dictionary[numbers] = new_person
+
+
+def number_search():
+    number_input = int_input("enter number: ")
+    for number, data in dictionary.items():
+        if number_input == number:
+            name = data["name"]
+            age = data["age"]
+            address = data["address"]
+            print(f"Nr.: {number}" + "\n" + f"Name: {name}" + "\n" + f"Age: {age}" + "\n" + f"Address: {address}")
 
 
 def name_search():
     name_input = input("enter name: ")
-    for name, data in dictionary.items():
-        if name_input in name:
+    for number, data in dictionary.items():
+        if name_input in data["name"]:
+            name = data["name"]
             age = data["age"]
             address = data["address"]
-            print(f"Name: {name}" + "\n" + f"Age: {age}" + "\n" + f"Address: {address}")
+            print(f"Nr.: {number}" + "\n" + f"Name: {name}" + "\n" + f"Age: {age}" + "\n" + f"Address: {address}")
 
 
 def age_search():
-    age_input = int(input("enter age: "))
-    for name, data in dictionary.items():
-        if age_input in data["age"]:
+    age_input = int_input("enter age: ")
+    for number, data in dictionary.items():
+        if age_input == data["age"]:
+            name = data["name"]
             age = data["age"]
             address = data["address"]
-            print(f"Name: {name}" + "\n" + f"Age: {age}" + "\n" + f"Address: {address}")
+            print(f"Nr.: {number}" + "\n" + f"Name: {name}" + "\n" + f"Age: {age}" + "\n" + f"Address: {address}")
 
 
 def address_search():
     address_input = input("enter address: ")
-    for name, data in dictionary.items():
+    for number, data in dictionary.items():
         if address_input in data["address"]:
+            name = data["name"]
             age = data["age"]
             address = data["address"]
-            print(f"Name: {name}" + "\n" + f"Age: {age}" + "\n" + f"Address: {address}")
+            print(f"Nr.: {number}" + "\n" + f"Name: {name}" + "\n" + f"Age: {age}" + "\n" + f"Address: {address}")
 
 
 def change():
-    change_input = input("change the data of: ")
-    for name, value in dictionary.items():
-        if change_input == name:
-            print(dictionary[name])
+    change_input = int_input("number of the person: ")
+    for number, data in dictionary.items():
+        if change_input == number:
+            print(dictionary[number])
             change_data = input("which data do you want to change: ")
             if "name" in change_data:
                 name_change = input("change name to: ")
-                dictionary[name_change] = dictionary.pop(change_input)
+                dictionary[change_input]["name"] = name_change
             if "age" in change_data:
-                age_change = int(input("change age to: "))
+                age_change = int_input("age: ")
                 dictionary[change_input]["age"] = age_change
             if "address" in change_data:
                 address_change = input("change address to: ")
                 dictionary[change_input]["address"] = address_change
 
 
+def int_input(text):
+    while True:
+        try:
+            age = int(input(text))
+            return age
+        except ValueError:
+            print("please enter a number")
+            continue
+
+
 while True:
     command = input("enter command: ")
     if "list" in command:
-        print(dictionary)
+        for number, data in dictionary.items():
+            print(number, data)
     if "add" in command:
         add_data()
     if "change" in command:
         change()
     if "search" in command:
         search_input = input("search for: ")
+        if "number" in search_input:
+            number_search()
         if "name" in search_input:
             name_search()
         if "age" in search_input:
